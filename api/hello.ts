9dc2749c0b89ab658e6f7e141b28e8a062ffcd1d
Oleg Loutchansky <luchansodev@gmail.com>
2021-03-16 18:10:12 +0300

    feat: experiments

--- api/hello.ts
+++ api/hello.ts
@@ -13,31 +13,32 @@ export default (request: VercelRequest, response: VercelResponse) => {
     test.push(chunk);
   }).on('end', () => {
     test = Buffer.concat(test).toString();
+    console.log(test);
+    response.status(400).send("Unknown command");
     // at this point, `body` has the entire request body stored in it as a string
   });
-  console.log(test);
 
 
-  if (checkAuth(request, response, PUBLIC_KEY) !== OK) {
-    return;
-  }
+  // if (checkAuth(request, response, PUBLIC_KEY) !== OK) {
+  //   return;
+  // }
 
-  const BOT_TOKEN = process.env.BOT_TOKEN;
-  const body = request.body;
+  // const BOT_TOKEN = process.env.BOT_TOKEN;
+  // const body = request.body;
 
-  console.log('-'.repeat(50));
-  console.log(body);
+  // console.log('-'.repeat(50));
+  // console.log(body);
 
-  if (!isInteractiveBody(body)) {
-    console.log('isInteractiveBody false - 400 error');
-    return response.status(400);
-  }
+  // if (!isInteractiveBody(body)) {
+  //   console.log('isInteractiveBody false - 400 error');
+  //   return response.status(400);
+  // }
 
-  if (body.type === PING) {
-    console.log('BODY ping - 200');
-    return response.status(200).send({ type: PING });
-  }
+  // if (body.type === PING) {
+  //   console.log('BODY ping - 200');
+  //   return response.status(200).send({ type: PING });
+  // }
 
-  console.log('Unknown command - 400 error');
-  response.status(400).send("Unknown command");
+  // console.log('Unknown command - 400 error');
+  // response.status(400).send("Unknown command");
 };
