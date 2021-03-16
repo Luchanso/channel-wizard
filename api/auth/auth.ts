9cd7436e49d3e0cd6d52db4919fbc03c542f2b14
Oleg Loutchansky <luchansodev@gmail.com>
2021-03-16 18:16:41 +0300

    fix: body parse

--- api/auth/auth.ts
+++ api/auth/auth.ts
@@ -3,7 +3,7 @@ import nacl from 'tweetnacl';
 import { OK } from "../constants/constants";
 
 export const checkAuth = (request: VercelRequest, response: VercelResponse, publicKey: string) => {
-  const body = request.body;
+  const body = JSON.stringify(request.body);
   const signature = request.headers['x-signature-ed25519'] as string;
   const timestamp = request.headers['x-signature-timestamp'] as string;
 
--- api/hello.ts
+++ api/hello.ts
@@ -6,41 +6,26 @@ import { isInteractiveBody } from "./types/types";
 export default (request: VercelRequest, response: VercelResponse) => {
   const PUBLIC_KEY = process.env.PUBLIC_KEY;
 
-  const temp = JSON.stringify(request.body);
-  console.log(temp);
+  if (checkAuth(request, response, PUBLIC_KEY) !== OK) {
+    return;
+  }
 
-  let test: any = [];
-  request.on('data', (chunk) => {
-    test.push(chunk);
-  }).on('end', () => {
-    test = Buffer.concat(test).toString();
-    console.log(test);
-    console.log(test === temp);
-    response.status(400).send("Unknown command");
-    // at this point, `body` has the entire request body stored in it as a string
-  });
+  const BOT_TOKEN = process.env.BOT_TOKEN;
+  const body = request.body;
 
+  console.log('-'.repeat(50));
+  console.log(body);
 
-  // if (checkAuth(request, response, PUBLIC_KEY) !== OK) {
-  //   return;
-  // }
+  if (!isInteractiveBody(body)) {
+    console.log('isInteractiveBody false - 400 error');
+    return response.status(400);
+  }
 
-  // const BOT_TOKEN = process.env.BOT_TOKEN;
-  // const body = request.body;
+  if (body.type === PING) {
+    console.log('BODY ping - 200');
+    return response.status(200).send({ type: PING });
+  }
 
-  // console.log('-'.repeat(50));
-  // console.log(body);
-
-  // if (!isInteractiveBody(body)) {
-  //   console.log('isInteractiveBody false - 400 error');
-  //   return response.status(400);
-  // }
-
-  // if (body.type === PING) {
-  //   console.log('BODY ping - 200');
-  //   return response.status(200).send({ type: PING });
-  // }
-
-  // console.log('Unknown command - 400 error');
-  // response.status(400).send("Unknown command");
+  console.log('Unknown command - 400 error');
+  response.status(400).send("Unknown command");
 };
