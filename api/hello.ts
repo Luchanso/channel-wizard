bd55ab6caab7aaf984d818b526f48cbc84bfc6ec
Oleg Loutchansky <luchansodev@gmail.com>
2021-03-16 13:08:06 +0300

    feat: add html

--- api/hello.ts
+++ api/hello.ts
@@ -1,6 +1,9 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default (request: VercelRequest, response: VercelResponse) => {
-  const { name = "World" } = request.query;
-  response.status(200).send(`Hello ${name}!`);
+  const result = {
+    body: request.body,
+    query: request.query
+  }
+  response.status(200).send(JSON.stringify(result, null, 2));
 };
